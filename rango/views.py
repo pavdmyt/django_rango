f047f6c580cceafe6d5f03dc177d54d155c2ba4a
pavdmyt <pavdmyt@users.noreply.github.com>
2015-05-20 21:45:39 +0300

    5.6 Exercises. Done.

--- rango/views.py
+++ rango/views.py
@@ -11,5 +11,4 @@ def index(request):
 
 
 def about(request):
-    return HttpResponse("""Rango says here is the about page.
-                           <a href='/rango/'>Index</a>""")
+    return render(request, 'rango/about.html')
