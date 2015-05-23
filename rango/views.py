4938749bd32f69e2a5a2976cfa524cfb609a5bad
pavdmyt <pavdmyt@users.noreply.github.com>
2015-05-23 16:43:30 +0300

    7.2 views.py and index.html modified to show the top five rango`ed categories.

--- rango/views.py
+++ rango/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from rango.models import Category
 
 
 def index(request):
-    # dict that passed to template engine as its context
-    context_dict = {'boldmessage': "I am bold font  from the context"}
+    # Query the database for a list of ALL categories currently stored.
+    # Order the categories by no. likes in descending order.
+    # Retrieve the top 5 only - or all if less than 5.
+    # Place the list in our `context_dict` which will be passed to the template engine.
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
 
     # render the response
     return render(request, 'rango/index.html', context_dict)
