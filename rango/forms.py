3227614d114b144ddca47480484e232dac6b9fba
pavdmyt <pavdmyt@users.noreply.github.com>
2015-05-28 00:09:59 +0300

    8.2.7 Cleaner Forms. clean() method added to PageForm.

--- rango/forms.py
+++ rango/forms.py
@@ -23,6 +23,23 @@ class PageForm(forms.ModelForm):
     url = forms.URLField(max_length=200, help_text="Please enter the URL of the page.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
+    # In scenarios where user input may not be entirely correct,
+    # we can override the the `clean()` method.
+    # This method is called upon before saving form data to a new model
+    # instance, and thus provides us with a logical place to insert
+    # code which can verify - and even fix - any form data the user inputs.
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+
+        # If url is not empty and doesn't start with 'http://', prepend 'http://'
+        if url and not url.startswith('http://'):
+            url = 'http://' + url
+            cleaned_data['url'] = url
+
+        return cleaned_data
+
     class Meta:
         # provide an association between the ModelForm and a model
         model = Page
