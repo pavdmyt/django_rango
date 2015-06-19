d3d0457440d129d5796d9d674ce31f71f44b46ca
pavdmyt <pavdmyt@users.noreply.github.com>
2015-06-19 16:10:33 +0300

    12.3.7 When users register, redirect them to index page.

--- django_rango/urls.py
+++ django_rango/urls.py
@@ -2,6 +2,13 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings  # for media server
 
+from registration.backends.simple.views import RegistrationView
+
+
+class MyRegistrationView(RegistrationView):
+    def get_success_url(self, request, user):
+        return '/rango/'
+
 
 urlpatterns = patterns('',
     # Examples:
@@ -10,6 +17,8 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^rango/', include('rango.urls')),
+    url(r'^accounts/register/$', MyRegistrationView.as_view(),
+        name='registration_register'),
     (r'^accounts/', include('registration.backends.simple.urls')),
 )
 
