373d00dd8031e4d1b004b5763c230096c641fcb0
pavdmyt <pavdmyt@users.noreply.github.com>
2015-06-24 14:21:10 +0300

    14.3 Current category highlight via parameterised Template Tag.

--- rango/templatetags/rango_extras.py
+++ rango/templatetags/rango_extras.py
@@ -6,5 +6,5 @@ register = template.Library()
 
 
 @register.inclusion_tag('rango/cats.html')
-def get_category_list():
-    return {'cats': Category.objects.all()}
+def get_category_list(cat=None):
+    return {'cats': Category.objects.all(), 'act_cat': cat}
